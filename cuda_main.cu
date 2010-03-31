c16ccca1eb415c7871994b4ca5925a305b712452
goering <goering@winnie.(none)>
2010-04-01 01:53:40 +0200

    reduce in eigene funktion ausgegliedert

--- cuda_main.cu
+++ cuda_main.cu
@@ -13,7 +13,15 @@
 #include "cuda_kernel.cu"
 #include "cuda_utils.h"
 
-void runFindMax(int dimGrid, int dimBlock, int* d_reduction_index, float* d_reduction_value, int set, int first, int data_size1)
+const int maxNumThreadsPerBlock = 128;
+
+// variables for reduction function
+int reduction_length[2];
+float *d_reduction_value[2];
+int *d_reduction_index[2];
+int reduction_nthreads[2];
+
+void reduction_startKernel(int dimGrid, int dimBlock, int* d_reduction_index, float* d_reduction_value, int set, int first, int data_size1)
 {
 	switch (dimBlock)
 	{
@@ -42,6 +50,104 @@ void runFindMax(int dimGrid, int dimBlock, int* d_reduction_index, float* d_redu
 }
 
 
+void reduction_init()
+{
+	reduction_nthreads[0] = maxNumThreadsPerBlock;
+	reduction_nthreads[1] = maxNumThreadsPerBlock;
+
+	int i;
+	for(i=0;i<2;i++)
+	{
+		reduction_length[i] = (( prob[i].l - 1) / (2 * reduction_nthreads[i])) + 1;
+
+		while(reduction_nthreads[i] > prob[i].l)
+		{
+			reduction_nthreads[i] /= 2;
+		}
+
+		cutilSafeCall(cudaMalloc((void**) &d_reduction_value[i], reduction_length[i] * sizeof(float)));
+		cutilSafeCall(cudaMalloc((void**) &d_reduction_index[i], reduction_length[i] * sizeof(int)));
+	}
+
+	printf(" rl: %d, thr: %d prob: %d and rl: %d thr: %d prob: %d \n", reduction_length[0], reduction_nthreads[0], prob[0].l, reduction_length[1], reduction_nthreads[1], prob[1].l);
+}
+
+
+void reduction_findMaximum()
+{
+	reduction_startKernel(reduction_length[0], reduction_nthreads[0], d_reduction_index[0], d_reduction_value[0], 0, 1,0);
+	reduction_startKernel(reduction_length[1], reduction_nthreads[1], d_reduction_index[1], d_reduction_value[1], 1, 1,0);
+
+	//reduce6<256><<<reduction_length[1], reduction_nthreads[1]>>>(d_reduction_index[1], d_reduction_value[1], 1, 1, 0);
+	cudaThreadSynchronize();
+
+	int temp_reduction_length[2];
+	int temp_reduction_nthreads[2];
+
+	int k;
+	for(k=0;k<2;k++)			 //todo: diesen hack entfernen
+	{
+		temp_reduction_length[k] = reduction_length[k];
+		temp_reduction_nthreads[k] = reduction_nthreads[k];
+	}
+
+	int counter = 0;
+	while( (reduction_length[0] != 1) |  (reduction_length[1] != 1) )
+	{
+		counter++;
+		int j;
+		for(j=0;j<2;j++)
+		{
+			if(reduction_length[j] != 1)
+			{
+				int data_size = reduction_length[j];
+
+				reduction_nthreads[j] = 256;
+				reduction_length[j] = (( data_size - 1) / (2 * reduction_nthreads[j])) + 1;
+
+				while(reduction_nthreads[j] > data_size)
+				{
+					reduction_nthreads[j] /= 2;
+				}
+
+				reduction_startKernel(reduction_length[j], reduction_nthreads[j], d_reduction_index[j], d_reduction_value[j], j, 0, data_size);
+			}
+		}
+		cudaThreadSynchronize();
+	}
+
+	for(k=0;k<2;k++)
+	{
+		reduction_length[k] = temp_reduction_length[k];
+		reduction_nthreads[k] = temp_reduction_nthreads[k];
+	}
+
+	/*		float reduction[2];
+			int reduction_idx[2];
+			cutilSafeCall(cudaMemcpy( &reduction, d_reduction_value[0], sizeof(float) * 1, cudaMemcpyDeviceToHost));
+			cutilSafeCall(cudaMemcpy( &reduction_idx, d_reduction_index[0], sizeof(int), cudaMemcpyDeviceToHost));
+			int j;
+			for(j=0;j<1;j++)
+				printf(" %d : %f - %d \n", j, reduction[j], reduction_idx[j]);
+	*/
+	float max1[1];
+	float max2[1];
+	int max1_idx[1];
+	int max2_idx[1];
+	cutilSafeCall(cudaMemcpy( &max1, d_reduction_value[0], sizeof(float), cudaMemcpyDeviceToHost));
+	cutilSafeCall(cudaMemcpy( &max2, d_reduction_value[1], sizeof(float), cudaMemcpyDeviceToHost));
+	cutilSafeCall(cudaMemcpy( &max1_idx, d_reduction_index[0], sizeof(int), cudaMemcpyDeviceToHost));
+	cutilSafeCall(cudaMemcpy( &max2_idx, d_reduction_index[1], sizeof(int), cudaMemcpyDeviceToHost));
+
+	printf("max[0] = %d (%f)   max[1] = %d (%f)  \n", max1_idx[0], max1[0], max2_idx[0], max2[0]);
+
+	cudaThreadSynchronize();
+
+	// check if kernel execution generated and error
+	cutilCheckMsg("Kernel execution failed");
+}
+
+
 /**
  * @brief Eine kurze Beschreibung der Funktion
  *
@@ -58,8 +164,8 @@ extern "C" void run_cuda_kernel()
 
 	// set number of blocks, and threads per block
 
-	nblocks  = ((prob[0].l + prob[1].l) / 256) + 1;
-	nthreads = 256;
+	nblocks  = ((prob[0].l + prob[1].l - 1) / maxNumThreadsPerBlock) + 1;
+	nthreads = maxNumThreadsPerBlock;
 	printf("blocks: %d \n", nblocks);
 
 	// allocate memory for array
@@ -80,12 +186,10 @@ extern "C" void run_cuda_kernel()
 		cutilSafeCall(cudaMalloc((void **)&d_weights[i], sizeof(float) * prob[i].l));
 								 //todo: speicher wieder freigeben.
 		h_weights[i] = (float*) malloc(sizeof(float) * prob[i].l);
-		/*
-		 * @todo : hier mal ein test-todo
-		 */
 		memset( (void**) h_data_temp , 0, size_of_data);
 								 // 	 \todo : : int 0 == float 0?
-		memset( (void**) h_weights[i] , 10, sizeof(float) * prob[i].l);
+
+		// copy data from data structure to plain array
 		int j;
 		for(j=0;j<prob[i].l; j++)
 		{
@@ -113,30 +217,7 @@ extern "C" void run_cuda_kernel()
 								 // 	 \todo : : prob.l durch was schoeners  ersetzen?
 	cutilSafeCall(cudaMalloc((void**) &d_dot_yi_y, prob[1].l * sizeof(float)));
 
-	// find max power of 2 that is smaller than prob.l
-	int reduction_length[2];
-	float *d_reduction_value[2];
-	int *d_reduction_index[2];
-	int reduction_nthreads[2];
-	reduction_nthreads[0] = 256;
-	reduction_nthreads[1] = 256; //todo: reduce.pdf sagt am besten 128
-
-	for(i=0;i<2;i++)
-	{
-		reduction_length[i] = (( prob[i].l - 1) / (2 * reduction_nthreads[i])) + 1;
-
-		while(reduction_nthreads[i] > prob[i].l)
-		{
-			reduction_nthreads[i] /= 2;
-		}
-
-		//reduction_length[i] = 128; //test
-
-		cutilSafeCall(cudaMalloc((void**) &d_reduction_value[i], reduction_length[i] * sizeof(float)));
-		cutilSafeCall(cudaMalloc((void**) &d_reduction_index[i], reduction_length[i] * sizeof(int)));
-	}
-
-	printf(" rl: %d, thr: %d prob: %d and rl: %d thr: %d prob: %d \n", reduction_length[0], reduction_nthreads[0], prob[0].l, reduction_length[1], reduction_nthreads[1], prob[1].l);
+	reduction_init();
 
 	//cache parameters
 	nr_of_cache_entries = 4;
@@ -162,7 +243,7 @@ extern "C" void run_cuda_kernel()
 	float* d_temp;
 	cutilSafeCall(cudaMalloc( (void**) &d_temp, sizeof( float* ) * temp_size));
 
-	// execute kernel
+	// execute kernels
 
 	cuda_kernel_init_pointer<<<1,1>>>(d_data[0], d_data[1], max_index, prob[0].l, prob[1].l,
 		d_weights[0], d_weights[1],
@@ -182,7 +263,8 @@ extern "C" void run_cuda_kernel()
 	{
 		cuda_kernel_lambda<<<1, 1>>>();
 		cudaThreadSynchronize();
-		cuda_kernel_updateWeights<<< nblocks, nthreads >>>(); //todo: nur soviele kernel starten wie wirklich noetig
+								 //todo: nur soviele kernel starten wie wirklich noetig
+		cuda_kernel_updateWeights<<< nblocks, nthreads >>>();
 		cudaThreadSynchronize();
 		// check if kernel execution generated and error
 		cutilCheckMsg("Kernel execution failed");
@@ -191,118 +273,8 @@ extern "C" void run_cuda_kernel()
 		cudaThreadSynchronize();
 		// check if kernel execution generated and error
 		cutilCheckMsg("Kernel execution failed");
-		//		cuda_kernel_distance<<<1, 1>>>();
 
-		float *d_temp1;
-		cutilSafeCall(cudaMalloc((void**) &d_temp1, 1024 * sizeof(float)));
-
-		int *d_temp2;
-		cutilSafeCall(cudaMalloc((void**) &d_temp2, 1024 * sizeof(int)));
-
-		runFindMax(reduction_length[0], reduction_nthreads[0], d_reduction_index[0], d_reduction_value[0], 0, 1,0);
-		runFindMax(reduction_length[1], reduction_nthreads[1], d_reduction_index[1], d_reduction_value[1], 1, 1,0);
-
-		//reduce6<256><<<reduction_length[1], reduction_nthreads[1]>>>(d_reduction_index[1], d_reduction_value[1], 1, 1, 0);
-		cudaThreadSynchronize();
-
-		int tempSize = 512;
-		float h_temp1[tempSize];
-		cutilSafeCall(cudaMemcpy( &h_temp1, d_temp1, sizeof(float) * tempSize, cudaMemcpyDeviceToHost));
-		int h_temp2[tempSize];
-		cutilSafeCall(cudaMemcpy( &h_temp2, d_temp2, sizeof(int) * tempSize, cudaMemcpyDeviceToHost));
-
-		int l;
-		for(l=0;l<3;l++)
-		{
-			//printf("%d - %f (%d)\n", l, h_temp1[l], h_temp2[l]);
-		}
-
-		//reduce6<2><<<1, 2>>>(d_reduction_index[1], d_reduction_value[1], 1, 0, 3);
-		//		runFindMax(1, 2, d_reduction_index[1], d_reduction_value[1], 1, 0);
-
-		float values[4];
-		int indizes[4];
-
-		cutilSafeCall(cudaMemcpy( &values, d_reduction_value[1], sizeof(float) * 4, cudaMemcpyDeviceToHost));
-		cutilSafeCall(cudaMemcpy( &indizes, d_reduction_index[1], sizeof(int) * 4, cudaMemcpyDeviceToHost));
-
-		int k;
-		for(k=0;k<1;k++)
-		{
-			//printf(" %d: %f (%d) \n", k, values[k], indizes[k]);
-		}
-
-		int temp_reduction_length[2];
-		int temp_reduction_nthreads[2];
-
-		for(k=0;k<2;k++)		 //todo: diesen hack entfernen
-		{
-			temp_reduction_length[k] = reduction_length[k];
-			temp_reduction_nthreads[k] = reduction_nthreads[k];
-		}
-
-		int counter = 0;
-		while( (reduction_length[0] != 1) |  (reduction_length[1] != 1) )
-		{
-			counter++;
-			int j;
-			for(j=0;j<2;j++)
-			{
-				if(reduction_length[j] != 1)
-				{
-					int data_size = reduction_length[j];
-
-					reduction_nthreads[j] = 256;
-					reduction_length[j] = (( data_size - 1) / (2 * reduction_nthreads[j])) + 1;
-
-					while(reduction_nthreads[j] > data_size)
-					{
-						reduction_nthreads[j] /= 2;
-					}
-
-					runFindMax(reduction_length[j], reduction_nthreads[j], d_reduction_index[j], d_reduction_value[j], j, 0, data_size);
-					//printf(" search max, iteration %d, teil %d,  %d-%d \n", counter, j, reduction_length[j], reduction_nthreads[j]);
-				}
-			}
-			cudaThreadSynchronize();
-		}
-
-		for(k=0;k<2;k++)
-		{
-			reduction_length[k] = temp_reduction_length[k];
-			reduction_nthreads[k] = temp_reduction_nthreads[k];
-		}
-
-		/*		float reduction[2];
-				int reduction_idx[2];
-				cutilSafeCall(cudaMemcpy( &reduction, d_reduction_value[0], sizeof(float) * 1, cudaMemcpyDeviceToHost));
-				cutilSafeCall(cudaMemcpy( &reduction_idx, d_reduction_index[0], sizeof(int), cudaMemcpyDeviceToHost));
-				int j;
-				for(j=0;j<1;j++)
-					printf(" %d : %f - %d \n", j, reduction[j], reduction_idx[j]);
-		*/
-		float max1[1];
-		float max2[1];
-		int max1_idx[1];
-		int max2_idx[1];
-		cutilSafeCall(cudaMemcpy( &max1, d_reduction_value[0], sizeof(float), cudaMemcpyDeviceToHost));
-		cutilSafeCall(cudaMemcpy( &max2, d_reduction_value[1], sizeof(float), cudaMemcpyDeviceToHost));
-		cutilSafeCall(cudaMemcpy( &max1_idx, d_reduction_index[0], sizeof(int), cudaMemcpyDeviceToHost));
-		cutilSafeCall(cudaMemcpy( &max2_idx, d_reduction_index[1], sizeof(int), cudaMemcpyDeviceToHost));
-
-		printf("max[0] = %d (%f)   max[1] = %d (%f)  \n", max1_idx[0], max1[0], max2_idx[0], max2[0]);
-
-		//printf("aufruf zweite runde\n");
-
-		//runFindMax(1, reduction_length[0] / 2, d_reduction_index[0], d_reduction_value[0], 0, 0);
-		//runFindMax(1, reduction_length[1] / 2, d_reduction_index[1], d_reduction_value[1], 1, 0);
-
-		cudaThreadSynchronize();
-
-		//printf("d_reduction_index[0][0] = %d \n", d_reduction_index[0][0]);
-		//printf("d_reduction_index[1][0] = %d \n", d_reduction_index[1][0]);
-		// check if kernel execution generated and error
-		cutilCheckMsg("Kernel execution failed");
+		reduction_findMaximum();
 	}
 
 	// copy back results and print them
@@ -323,6 +295,8 @@ extern "C" void run_cuda_kernel()
 				printf(" %d:%f \n", j, h_weights[i][j]);
 		}
 	}
+
+	//todo: free memory
 
 	// free memory
 
