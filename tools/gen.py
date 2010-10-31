f00a34b622755272c6b729ce344ac64c8d5f151c
sietschie <sietschie@gmail.com>
2010-10-31 18:23:00 +0100

    fixed error

--- tools/gen.py
+++ tools/gen.py
@@ -34,7 +34,7 @@ vectors = int( int( sys.argv[2] ) / 2 )
 
 filename = 'rand-e' + str(elements) + '-v' + str(2*vectors)
 
-if len(sys.argv[3]) >= 4 and sys.argv[3] == 'varydensity':
+if len(sys.argv) >= 4 and sys.argv[3] == 'varydensity':
 	prob_list = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4,0.3,0.2, 0.5, 0.1, 0.09, 0.08, 0.07, 0.06, 0.05, 0.04, 0.03, 0.02, 0.01, 0.005, 0.001, 0.0001]
 else:
 	prob_list = [1]
